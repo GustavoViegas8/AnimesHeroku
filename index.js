f0c87ca35dbf99491ced909f27e6aac7ef4a5967
Gustavo Viegas <gustavoviegas231914@gmail.com>
2021-05-24 22:11:26 -0300

    Teste Deploy

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
+require('dotenv').config()
 const routes = require('./routes');
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json())
-app.use(routes)
 
 app.get('/', (req, res) => {
 
  res.send('Hello World!')
 
 })
+
+app.use(express.json())
+app.use(routes)
 
 app.listen(port, () => {
  console.log(`Servidor rodando em localhost:${port}`)
--- routes.js
+++ routes.js
@@ -1,26 +1,27 @@
 const express = require('express');
-const routes = express.Router()
+const routes = express.Router();
 
-const userController = require('./controllers/usuario')
-const loginTest = require('./middlewares/jwtLogin')
-const login = require('./controllers/userLogin')
+const userController = require('./controllers/usuario');
+const loginTest = require('./middlewares/jwtLogin');
+const login = require('./controllers/userLogin');
 
-const anime = require('./controllers/anime')
+const anime = require('./controllers/anime');
 
+routes
+    .post('/userLogin', login.usuarioLogin)
+    .get('/showUsers', loginTest, userController.mostrarUsuarios)
+    .post('/userRegister', userController.cadastroUsuario)
+    .put('/userUpdate/:id', loginTest, userController.trocarSenha)
+    .delete('/userDelete/:id', loginTest, userController.deletarUser);
 
-routes.post('/userLogin', login.usuarioLogin)
-routes.get('/showUsers', loginTest, userController.mostrarUsuarios)
-routes.post('/userRegister', userController.cadastroUsuario)
-routes.put('/userUpdate/:id', loginTest, userController.trocarSenha)
-routes.delete('/userDelete/:id', loginTest, userController.deletarUser)
-
-routes.get('/animeSimples', loginTest, anime.animesSimples)
-routes.get('/anime', loginTest, anime.animes)
-routes.post('/addAnime/:id', loginTest, anime.addAnime)
-routes.put('/updateAnime/:id', loginTest, anime.updateAnime)
-routes.delete('/deleteAnime/:id', loginTest, anime.deleteAnime)
-routes.get('/destaques', loginTest, anime.destaques)
-routes.post('/destaques/:id', loginTest, anime.animeDestaque)
-routes.get('/filtro/:palavra',loginTest, anime.filtro)
+routes
+    .get('/animeSimples', loginTest, anime.animesSimples)
+    .get('/anime', loginTest, anime.animes)
+    .post('/addAnime/:id', loginTest, anime.addAnime)
+    .put('/updateAnime/:id', loginTest, anime.updateAnime)
+    .delete('/deleteAnime/:id', loginTest, anime.deleteAnime)
+    .get('/destaques', loginTest, anime.destaques)
+    .post('/destaques/:id', loginTest, anime.animeDestaque)
+    .get('/filtro/:palavra',loginTest, anime.filtro);
 
 module.exports = routes;
